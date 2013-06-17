cf40a463c3e53fa723ea814a397daec38f755d44
ng_chi_wa@hotmail.com <ng_chi_wa@hotmail.com>
2013-06-17 19:47:31 +0800

    init Twitter APP

--- src/com/codepath/apps/restclienttemplate/LoginActivity.java
+++ src/com/codepath/apps/restclienttemplate/LoginActivity.java
@@ -1,15 +1,27 @@
 package com.codepath.apps.restclienttemplate;
 
+import java.io.Serializable;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
 import com.codepath.oauth.OAuthLoginActivity;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
 
-public class LoginActivity extends OAuthLoginActivity<RestClient> {
+public class LoginActivity extends OAuthLoginActivity<TwitterClient> {
+
+	TwitterClient twitterClient;
+	Intent i;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// twitterClient = new TwitterClient(this.getBaseContext());
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 	}
@@ -20,27 +32,28 @@ public class LoginActivity extends OAuthLoginActivity<RestClient> {
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
-	
+
 	// OAuth authenticated successfully, launch primary authenticated activity
 	// i.e Display application "homepage"
-    @Override
-    public void onLoginSuccess() {
-    	// Intent i = new Intent(this, PhotosActivity.class);
-    	// startActivity(i);
-    }
-    
-    // OAuth authentication flow failed, handle the error
-    // i.e Display an error dialog or toast
-    @Override
-    public void onLoginFailure(Exception e) {
-        e.printStackTrace();
-    }
-    
-    // Click handler method for the button used to start OAuth flow
-    // Uses the client to initiate OAuth authorization
-    // This should be tied to a button used to login
-    public void loginToRest(View view) {
-        getClient().connect();
-    }
+	@Override
+	public void onLoginSuccess() {
+		i = new Intent(this, ListHomeTimeLineActivity.class);
+		startActivity(i);
+	}
+
+	// OAuth authentication flow failed, handle the error
+	// i.e Display an error dialog or toast
+	@Override
+	public void onLoginFailure(Exception e) {
+		e.printStackTrace();
+	}
+
+	// Click handler method for the button used to start OAuth flow
+	// Uses the client to initiate OAuth authorization
+	// This should be tied to a button used to login
+	public void loginToRest(View view) {
+		twitterClient = getClient();
+		twitterClient.connect();
+	}
 
 }
